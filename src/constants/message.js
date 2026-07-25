e84876e15ed1a64ee44ceba4cd21ca52107838ba
udaynarendra <udaynarendragudiguntla@gmail.com>
2026-07-25 23:08:29 +0530

    create common usage message in message object

---
+++ src/constants/message.js
@@ -0,0 +1,10 @@
+const message={
+    SUCCESS:'success',
+    FAILED:'failed',
+    SERVER_ERROR:'Internal Server Error',
+    CREATED:'Created Successfully',
+    UPDATED:'Updated Successfully',
+    DELETED:'Deleted Successfully',
+    FETCHED:'Data Retrieved Successfully'
+}
+export default message;
