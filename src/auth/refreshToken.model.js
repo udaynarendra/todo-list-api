f89d110e3e22e46a0a75d5486a16bda57fcca909
udaynarendra <udaynarendragudiguntla@gmail.com>
2026-07-25 21:54:53 +0530

    creating refreshToken Schema

---
+++ src/auth/refreshToken.model.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+const refreshTokenSchema=new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    token:{
+        type:String,
+        required:true
+    },
+    expiresAt:{
+        type:Date,
+        required:true
+    }
+},{
+    timestamps:true
+})
+const RefreshToken=mongoose.model('RefreshToken',refreshTokenSchema);
+export default RefreshToken;
