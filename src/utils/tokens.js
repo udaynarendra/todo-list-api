bfb08d6ae032e590bb4650d8a2ffff25d3172005
udaynarendra <udaynarendragudiguntla@gmail.com>
2026-08-15 15:37:23 +0530

    token.js created

---
+++ src/utils/tokens.js
@@ -0,0 +1,11 @@
+import jwt from 'jsonwebtoken';
+import env from '../config/env.js';
+const createRefreshToken=(id)=>{
+    return jwt.sign({id},env.REFRESH_TOKEN_SECRET,{expiresIn:env.REFRESH_TOKEN_EXPIRES});
+}
+const createAccessToken=(email,id,name)=>{
+    return jwt.sign({id,name,email},env.ACCESS_TOKEN_SECRET,{expiresIn:env.ACCESS_TOKEN_EXPIRES});
+}
+
+
+export {createAccessToken,createRefreshToken};
